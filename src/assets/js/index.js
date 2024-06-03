10d5f30429b82c2012c58eebbdb99c2296e1a05f
JustEnzoGang <enzorodrigues94340@gmail.com>
2024-06-03 21:35:31 +0200

    Publish version 1.0.2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "4Kraft est un server sur Minecraft.", "author": "JustEnzo" },
+            { "message": "Ce programme est en javascript.", "author": "JustEnzo" },
+            { "message": "Si tu fait /battlepass tu pourra ouvrir le pass de combat.", "author": "JustEnzo" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
